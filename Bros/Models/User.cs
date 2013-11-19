1f56d1a01c76be9f0151a55296615c04c78d67c5
ahomer <adamhomer88@gmail.com>
2013-11-19 15:36:16 -0700

    Created authentication methods for a user
    Created the GeneratedSaltedHash method which takes in a plain text
    password and a byte[] salt, hashes the salt and password together and
    returns the byte[] of the hash.
    
    Created CompareByteArrays which will be used to authenticate a users.
    Takes in the hashed, salted password of a user and compares it with the
    hashed, salted plain text password entered into the login form.
    
    Added the CreateSalt method which takes in a size for the salt and
    returns a Cryptographically random byte array.

--- Bros/Models/User.cs
+++ Bros/Models/User.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace Bros.Models
@@ -9,6 +11,8 @@ namespace Bros.Models
     {
 		public virtual Profile Profile { get; set; }
         public int ID { get; set; }
+        public byte[] password { get; set; }
+        public byte[] salt { get; set; }
 		public virtual List<Circle> Circles { get; set; }
         public string Email { get; set; }
 		public virtual List<BroRequest> SentBroRequests { get; set; }
@@ -17,5 +21,46 @@ namespace Bros.Models
 		public virtual List<Message> Messages { get; set; }
 		public virtual List<Post> Posts { get; set; }
         public virtual List<Comment> Comments { get; set; }
+
+        public static byte[] GeneratedSaltedHash(string plainText, byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA256Managed();
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[]  plainTextWithSaltedBytes = new byte[plainTextBytes.Length+salt.Length];
+
+            for (int i = 0; i < plainTextBytes.Length; i++)
+            {
+                plainTextWithSaltedBytes[i] = plainTextBytes[i];
+            }
+            for (int i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltedBytes[i + plainTextBytes.Length] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltedBytes);
+        }
+
+        public static bool CompareByteArrays(byte[] array1, byte[] array2)
+        {
+            if (array1.Length != array2.Length)
+                return false;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static byte[] CreateSalt(int size)
+        {
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[size];
+            rng.GetBytes(salt);
+
+            return salt;
+        }
     }
 }
