dbb6c33c7ebd30f5f7c25bce1d79bd71cd156d9d
Felix Milea-Ciobanu <felixmilea@gmail.com>
2013-11-25 15:06:37 -0700

    implemented basic status update

--- Bros/Controllers/ProfileController.cs
+++ Bros/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bros.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,35 @@ namespace Bros.Controllers
         {
             return View();
         }
+
+		public ActionResult Feed()
+		{
+			return View();
+		}
+
+		public new ActionResult Profile()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult PostStatus()
+		{
+			using (ModelFirstContainer context = new ModelFirstContainer())
+			{
+				User user = (User) Session["User"];
+
+				TextPost update = new TextPost() { Author = user, Content = Request["status"], DateCreated = new DateTime() };
+				user.Posts.Add(update);
+
+				context.SaveChanges();
+			}
+
+			if (Request.IsAjaxRequest())
+				return null;
+			else
+				return Redirect( Request.UrlReferrer.AbsolutePath );
+		}
 
     }
 }
