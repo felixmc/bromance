fe5d3ac4932a5254ee82976ac60ee00caf44e34d
Courtney Nguyen <3Solstice@gmail.com>
2013-12-01 17:37:47 -0700

    Added Messaging link, and working on messages

--- Bros/Controllers/MessagingController.cs
+++ Bros/Controllers/MessagingController.cs
@@ -15,15 +15,29 @@ namespace Bros.Controllers
 
         public ActionResult MessageHome()
         {
-            Bros.DataModel.User thisUser = (Bros.DataModel.User)Session["User"];
-            List<Message> allMessages = thisUser.SentMessages.ToList();
-            allMessages.AddRange(thisUser.ReceivedMessages.ToList());
 
-            allMessages = allMessages.OrderBy(x => x.DateSent).ToList();
-            List<User> uniqueUsers = allMessages.Select(m => m.Receiver).OrderBy(m => m.Id).Distinct().ToList();
-            ViewBag.messages = allMessages;
-            ViewBag.UserConversations = uniqueUsers;
-            return View();
+            if (Session["UserId"] != null)
+            {
+                int id = (int)Session["UserId"];
+                List<Message> allMessages;
+                using (var context = new ModelFirstContainer())
+                {
+                    User thisUser = context.Users.FirstOrDefault(x => x.Id == id);
+                    allMessages = thisUser.ReceivedMessages.ToList();
+                    //allMessages.AddRange(thisUser.ReceivedMessages.ToList());
+
+                    allMessages = allMessages.OrderBy(x => x.DateSent).ToList();
+                    List<User> uniqueUsers = allMessages.Select(m => m.Receiver).OrderBy(m => m.Id).Distinct().ToList();
+                    ViewBag.messages = allMessages;
+                    ViewBag.UserConversations = uniqueUsers;
+                }
+                return View("MessageHome", allMessages);
+            }
+            else
+            {
+                throw new Exception("Session not found, or not logged in.");
+                
+            }
         }
 
         public ActionResult LoadMessage(User u)
