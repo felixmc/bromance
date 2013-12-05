b88c50c547cce8bd226691dd90c5487c435ba0c0
Courtney Nguyen <3Solstice@gmail.com>
2013-12-05 15:45:40 -0700

    Upload profile picture works

--- Bros/Controllers/ProfileController.cs
+++ Bros/Controllers/ProfileController.cs
@@ -217,26 +217,6 @@ namespace Bros.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public ActionResult PostStatus()
-        //{
-        //    using (ModelFirstContainer context = new ModelFirstContainer())
-        //    {
-        //        int userId = (int)Session["UserId"];
-        //        User user = context.Users.Where(u => u.Id == userId).FirstOrDefault();
-
-        //        TextPost update = new TextPost() { Author = user, Content = Request["status"], DateCreated = DateTime.Now, DateUpdated = DateTime.Now };
-        //        user.Posts.Add(update);
-
-        //        context.SaveChanges();
-        //    }
-
-        //    if (Request.IsAjaxRequest())
-        //        return null;
-        //    else
-        //        return Redirect(Request.UrlReferrer.AbsolutePath);
-        //}
-
 		public ActionResult Post(int id)
 		{
 
@@ -308,35 +288,6 @@ namespace Bros.Controllers
 			else
 				return Redirect(Request.UrlReferrer.AbsolutePath);
 		}
-
-        //[HttpPost]
-        //public ActionResult PostComment()
-        //{
-        //    using (ModelFirstContainer context = new ModelFirstContainer())
-        //    {
-        //        int postId = Int32.Parse(Request["post"]);
-
-        //        Post post = context.Posts.Where(p => p.Id == postId).FirstOrDefault();
-
-        //        if (post != null)
-        //        {
-        //            int userId = (int)Session["UserId"];
-        //            User user = context.Users.Where(u => u.Id == userId).FirstOrDefault();
-        //            Comment comment = new Comment() { Content = Request["comment"], Owner = user, ParentPost = post, DateCreated = DateTime.Now };
-
-        //            user.Comments.Add(comment);
-        //            post.Comments.Add(comment);
-
-        //            context.SaveChanges();
-        //        }
-
-        //    }
-
-        //    if (Request.IsAjaxRequest())
-        //        return null;
-        //    else
-        //        return Redirect(Request.UrlReferrer.AbsolutePath);
-        //}
 
         [Authorize]
         [HttpPost]
@@ -477,9 +428,46 @@ namespace Bros.Controllers
 
         [Authorize]
         [HttpPost]
-        public ActionResult ChangeProfilePhoto(User user)
+        public ActionResult ChangeProfilePhoto(HttpPostedFileBase img)
         {
 
+            User loadUser = new User();
+            int userId = GetSessionInt();
+            using (var context = new ModelFirstContainer())
+            {
+                loadUser = context.Users.Include("Profile.ProfilePhoto").FirstOrDefault(u => u.Id == userId);
+
+
+                byte[] data = null;
+                if (img != null && img.ContentLength > 0)
+                {
+                    using (MemoryStream target = new MemoryStream())
+                    {
+                        img.InputStream.CopyTo(target);
+                        data = target.ToArray();
+                        loadUser.Profile.ProfilePhoto.ImageData = data;          
+                    }
+                }
+                else
+                {
+                    throw new Exception("derpp");
+                }
+                context.SaveChanges();
+            }
+            return View("Feed");
+        }
+
+        [Authorize]
+        public ActionResult ManageAlbums()
+        {
+            ICollection<Album> albumList = new List<Album>();
+            using(var context = new ModelFirstContainer()){
+                int id = GetSessionInt();
+                User user = context.Users.Include("Albums.Photos").FirstOrDefault(u => u.Id == id);
+                albumList = user.Albums;
+            }
+
+            ViewBag.Albums = albumList;
             return View();
         }
 
