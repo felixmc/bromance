621003ff2c0293e7443e4c49c0c7e61be0997e46
Felix Milea-Ciobanu <felixmilea@gmail.com>
2013-12-09 09:59:36 -0700

    implemented live chat system (and other small changes)

---
+++ Bros/Hubs/ChatUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bros.Hubs
+{
+	public class ChatUser
+	{
+		public int UserId { get; set; }
+		public List<int> Friends { get; set; }
+		public List<string> ConnectionIds { get; private set; }
+		
+		public ChatUser()
+		{
+			ConnectionIds = new List<string>();
+		}
+
+		public override bool Equals(System.Object obj)
+		{
+			if (obj == null) return false;
+
+			ChatUser u = obj as ChatUser;
+			if ((System.Object)u == null) return false;
+
+			return UserId == u.UserId;
+		}
+
+	}
+}
