0d82e79c266713d5becf213001751dc620eeafaf
Courtney Nguyen <3Solstice@gmail.com>
2013-12-14 17:01:18 -0700

    productfactory

---
+++ Bros/Models/ProductFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bros.Models
+{
+    public class ProductFactory : Controller
+    {
+        //
+        // GET: /ProductFactory/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
